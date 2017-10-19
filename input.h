f45376bba77f8f276c9c06625662381ab16f287d
Italo Ramon Campos <italo.ramon7@live.com>
2017-10-19 16:41:49 -0300

    Add files via upload
    Carregando alterações no main e no arquivo input.h relativas à função readFromUser.

--- input.h
+++ input.h
@@ -5,6 +5,17 @@
 
 using namespace std;
 
-void readFromUser(Lista &lista) {
-	
+void readFromUser(Tabela &tabela) {
+	Tabela *t = &tabela;
+	cout<<"LISTA DE ADJACENCIA ==============\n";
+	cout<<"Digite -1 para encerrar a leitura.\n\n";
+	t->newLine();
+	int valor = 0;
+	while(valor != -1) {
+		int i = 0;
+		cout<<"Linha #"<<i<<":";
+		cin>>valor;
+		t->pushItem(i, valor);
+		i++;
+	}
 }
--- main.cpp
+++ main.cpp
@@ -2,70 +2,23 @@
 #include <locale>
 #include "lista.h"
 #include "tabela.h"
+#include "input.h"
 
 using namespace std;
 
 int main() {
 	setlocale (LC_ALL, "portuguese");
-	/*Lista lista;
-	lista.addEnd(5);
-	lista.addEnd(10);
-	
-	Lista listaA;
-	listaA.addEnd(15);
-	listaA.addEnd(20);
-	listaA.addEnd(25);
-	lista.append(listaA);
-	lista.print();
-	//lista.printNodes();
-	lista.addPosition(2, 20);
-	lista.addPosition(0, 0);
-	lista.addPosition(6, 30);
-	lista.addPosition(3, -5);
-	//lista.printNodes();
-	/*lista.removeFirst();
-	lista.removeLast();
-	//lista.printNodes();
-	lista.removeAllItems(20);
-	lista.printNodes();
-	lista.removePosition(3);
-	lista.print();
-	lista.removePosition(0);
-	lista.print();
-	lista.removePosition(lista.length()-1);
-	lista.printNodes();
-	//lista.reverse();
-	lista.print();
-	cout<<"\nComprimento da lista: "<<lista.length();
-	//lista.removePosition(1);
-	//lista.print();*/
-	
-	/*Tabela table;
-	table.addList(lista);
-	cout<<"\nColunas: "<<table.numColumns(0);
-	cout<<"\nLinhas: "<<table.numLines();
-	cout<<"\nColunas: ";
-	table.numColumns();
-	table.print();
-	table.pushItem(20);
-	table.print();
-	table.setItemPosition(1, 0, 25);
-	table.print();
-	table.removeLastItem();
-	table.print();
-	table.dropLine();
-	table.print();
-	table.addList(listaA);
-	table.print();
-	table.dropTable();
-	table.print();*/
 	
 	Tabela fernoob;
 	Lista italo;
 	
+	//fernoob.print();
+	
+	readFromUser(fernoob);
+	
 	fernoob.print();
 	
-	for(int i=0; i<3; i++) {
+	/*for(int i=0; i<3; i++) {
 		for(int j=0; j<2; j++) {
 			printf("Informe a posicao [%d, %d]: ", i, j);
 			int temp;
@@ -92,7 +45,7 @@ int main() {
 	
 	cout<< endl << "This is an other fernoob: \n"; fernoob.print(); cout << endl;
 	
-	cout<<"\nGrau de saída do nó 1 = "<<fernoob.numColumns(1)-1;
+	cout<<"\nGrau de saída do nó 1 = "<<fernoob.numColumns(1)-1;*/
 	
 	
 	cout<<endl;
