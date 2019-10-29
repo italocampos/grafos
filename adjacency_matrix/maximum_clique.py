fb3d3daee27a85624279b55b2f1cfc3cc94dd826
Italo Ramon Campos <italo.ramon.campos@gmail.com>
2019-10-29 20:20:36 -0300

    Reordering paths

--- adjacency_matrix/maximum_clique.py
+++ adjacency_matrix/maximum_clique.py
@@ -1,67 +1,13 @@
 from graph import Graph
+from tools import convert_graph, merge_sort
 
-def remove_sublist(list_, sublist):
-	for element in sublist:
-		list_.remove(element)
-
-graph = Graph()
-graph.read_from_file('k26.in')
-
-
-def merge(vector_a, vector_b):
-	merged = list()
-	for _ in range(len(vector_a)):
-		if vector_a[0][1] > vector_b[0][1]:
-			merged.append(vector_a.pop(0))
-		else:
-			merged.append(vector_b.pop(0))
-	merged += vector_a + vector_b
-	return merged
-
-
-# Sort a list under form [('VERTEX', degree), ...] in a descending order
-def merge_sort(vector):
-	if len(vector) in [0, 1]:
-		return vector
-	else:
-		vector_a = merge_sort(vector[:len(vector)//2])
-		vector_b = merge_sort(vector[len(vector)//2:])
-		return merge(vector_a, vector_b)
+#graph = Graph()
+#graph.read_from_file('input/k26.in')
+graph = convert_graph('input/graph_250.in')
 
 
 maximal_clique = list()
 
-''' Version 1.0 (wrong)
-for vertex in graph.get_vertices():
-	adjacent = graph.get_adjacent(vertex)
-	not_clique = list()
-	begin = 1
-	for line in adjacent:
-		if line in not_clique:
-			break
-		for column in adjacent[begin:]:
-			if graph.get_edge(line, column) == 0:
-				#print('Do not compound the clique with %s: %d' % (vertex, line))
-				not_clique.append(line)
-				break
-		begin += 1
-
-	remove_sublist(adjacent, not_clique)
-	adjacent.insert(0, graph.get_vertex_position(vertex))
-	maximal_clique.append(adjacent)
-
-print(maximal_clique)
-
-maximum_clique = maximal_clique.pop(0)
-for clique in maximal_clique:
-	if len(clique) > len(maximum_clique):
-		maximum_clique = clique
-
-print('Maximum clique %d found at: ' % len(maximum_clique), end = '')
-for vertex in maximum_clique:
-	print(graph.get_vertex_name(vertex), ' ', end = '')
-print()
-'''
 
 for vertex in graph.get_vertices():
 	adjacent = graph.get_adjacent(vertex)
---
+++ adjacency_matrix/tools.py
@@ -0,0 +1,44 @@
+from graph import Graph
+
+def convert_graph(file_name):
+	file = open(file_name, 'r')
+	g = Graph()
+
+	for line in file:
+		if line != '\n':
+			a, b = line.split()
+			#a, b = int(a), int(b)
+			
+			if not g.vertex_exists(a):
+				g.add_vertex(a)
+			if not g.vertex_exists(b):
+				g.add_vertex(b)
+			g.add_edge(a, b)
+
+	return g
+
+
+def remove_sublist(list_, sublist):
+	for element in sublist:
+		list_.remove(element)
+
+
+def merge(vector_a, vector_b):
+	merged = list()
+	for _ in range(len(vector_a)):
+		if vector_a[0][1] > vector_b[0][1]:
+			merged.append(vector_a.pop(0))
+		else:
+			merged.append(vector_b.pop(0))
+	merged += vector_a + vector_b
+	return merged
+
+
+# Sort a list under form [('VERTEX', degree), ...] in a descending order
+def merge_sort(vector):
+	if len(vector) in [0, 1]:
+		return vector
+	else:
+		vector_a = merge_sort(vector[:len(vector)//2])
+		vector_b = merge_sort(vector[len(vector)//2:])
+		return merge(vector_a, vector_b)
